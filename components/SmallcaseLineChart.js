bd84add697bfc9481a08448fbf0fee972a12987b
Kashish Grover <kashishgrover95@gmail.com>
2018-02-06 11:46:53 +0530

    wip

--- components/SmallcaseLineChart.js
+++ components/SmallcaseLineChart.js
@@ -22,54 +22,49 @@ export class SmallcaseLineChart extends React.Component {
         this.dataPoints = points.map(x => x.index);
         this.max = Math.max.apply(Math, this.dataPoints);
         this.min = Math.min.apply(Math, this.dataPoints);
-
-        this.months = points.map(x => {
-            let y = new Date(x.date).getMonth() + '/' + new Date(x.date).getFullYear();
-            return y;
+        this.dates = points.map(x => {
+            let date = new Date(x.date);
+            date = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+            return date;
         });
-
-        alert(this.months)
-
         return true;
     }
 
     render() {
         return (
             !this.state.loading ?
-                <ScrollView
-                    style={{
-                        flexDirection: 'row',
-                        marginBottom: 24,
-                    }}
-                    pinchGestureEnabled={true}
-                    showsHorizontalScrollIndicator={false}
-                    showsVerticalScrollIndicator={false}
-                    horizontal={true}
-                    bouncesZoom={true}
-                    zoomScale={1}
-                >
-                    <AreaChart
+                <View>
+                    <ScrollView
                         style={{
-                            marginTop: 12,
-                            marginBottom: 48,
-                            width: 1200,
-                            height: 400,
-                            backgroundColor: '#ddd',
+                            flexDirection: 'row',
                         }}
-                        dataPoints={this.dataPoints}
-                        animate={true}
-                        showGrid={false}
-                        svg={{
-                            fill: 'rgba(134, 65, 244, 0.2)',
-                            stroke: 'rgb(134, 65, 244, 0.8)',
-                        }}
-                        contentInset={{ top: 20, bottom: 30 }}
-                    />
+                        pinchGestureEnabled={true}
+                        showsHorizontalScrollIndicator={false}
+                        showsVerticalScrollIndicator={false}
+                        horizontal={true}
+                        zoomScale={1}
+                    >
+                        <AreaChart
+                            style={{
+                                marginTop: 12,
+                                marginBottom: 24,
+                                width: 1200,
+                                height: 400,
+                                backgroundColor: '#ddd',
+                            }}
+                            dataPoints={this.dataPoints}
+                            animate={false}
+                            showGrid={false}
+                            svg={{
+                                fill: 'rgba(134, 65, 244, 0.2)',
+                                stroke: 'rgb(134, 65, 244, 0.8)',
+                            }}
+                            contentInset={{ top: 20, bottom: 30 }}
+                        />
+                    </ScrollView>
                     <YAxis
                         style={{ position: 'absolute', top: 0, bottom: 0 }}
                         dataPoints={this.dataPoints}
-                        min={this.min}
-                        max={this.max}
                         labelStyle={{
                             fontSize: 8,
                             color: 'black',
@@ -78,19 +73,15 @@ export class SmallcaseLineChart extends React.Component {
                     />
                     <XAxis
                         style={{ position: 'absolute', left: 0, bottom: 32, right: 0 }}
-                        values={this.months}
+                        values={this.dates}
                         labelStyle={{
                             fontSize: 12,
                             color: 'black',
                         }}
                         chartType={XAxis.Type.BAR}
-                        contentInset={{ top: 20, bottom: 30 }}
-                        formatLabel={(value, index) => {
-                            return value;
-                        }}
-
+                    // contentInset={{ top: 20, bottom: 30 }}
                     />
-                </ScrollView>
+                </View>
                 :
                 <View />
         );
